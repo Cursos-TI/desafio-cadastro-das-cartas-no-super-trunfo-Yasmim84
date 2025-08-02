c84e1ce5022e9672198be51a86caf6d9da8ec98f
Yasmim84 <yasmimprado83@gmail.com>
2025-08-02 18:41:30 +0000

    declarando as variáveis da primeira carta.

--- Cartas-Super-Trunfo.c
+++ Cartas-Super-Trunfo.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 
+int main () {
+    // Declaração das variáveis.
+    char Estado[4];
+    char Código [8];
+    char cidade [40];
+    int População, P_turístico;
+    float Área, PIB;
 
-int main() {
-    
+     // Informações sobre as cartas.
+
+     printf("Carta 01:\n\n");
+
+     printf("Informe qual é o estado da carta: ");
+     scanf("%s", Estado);
+
+     printf("Informe qual é codígo da carta: ");
+     scanf("%s", Código);
+
+     printf("Informe qual é o nome da cidade: ");
+     scanf("%s", cidade);
+
+     printf("Informe qual é a população desta carta: ");
+     scanf("%d", População);
+
+     printf("Informe qual a área em Km²: ");
+     scanf("%f", Área);
+
+     printf("Informe qual é o PIB desta carta: ");
+     scanf("%f", PIB);
+
+     printf("Informe quantos pontos turísticos essa carta possui: ");
+     scanf("%d", P_turístico);
+
+
+
+
+
+
+
 
     return 0;
 }
