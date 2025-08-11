e62717a3254b77967a44bfca022c041e3292e673
Yasmim84 <yasmimprado83@gmail.com>
2025-08-11 16:47:13 +0000

    nível aventureiro finalizado.

--- Cartas-Super-Trunfo.c
+++ Cartas-Super-Trunfo.c
@@ -10,7 +10,8 @@ int main() {
     int População1, População2;
     int P_turístico1, P_turístico2;
     float Área1, Área2;
-    float PIB1, PIB2;
+    float PIB1, PIB2, PIBpercapita1, PIBpercapita2;
+    float Densidade1, Densidade2;
      
     // Coleta de informações sobre a primeira carta 
 
@@ -64,16 +65,26 @@ int main() {
     printf("Informe quantos pontos turísticos: ");
     scanf("%d", &P_turístico2);
 
-    // saída com os dados coletados
+    // Cálculo da densidade populacional 
+    Densidade1 = População1 / Área1;
+    Densidade2 = População2 / Área2;
 
+    //Cálculo do PIB per capita
+    PIBpercapita1 = PIB1 / População1;
+    PIBpercapita2 = PIB2 / População2;
+
+    // saída com os dados coletados
+    
     printf("\n Dados da Carta 1:\n");
     printf("Código: %s\n", Código1);
     printf("Estado: %s\n", Estado1);
     printf("Nome da Cidade: %s", Cidade1);
     printf("População: %d\n", População1);
     printf("Área: %.2f Km²\n", Área1);
-    printf("PIB: %.2f\n", PIB1);
+    printf("PIB: %f\n", PIB1);
     printf("Número de Pontos Turísticos: %d\n\n", P_turístico1);
+    printf("Densidade Populacional: %f hab/Km²\n", Densidade1);
+    printf("PIB per Capita: %f reais\n", PIBpercapita1);
 
     printf("\n Dados da Carta 2:\n");
     printf("Código: %s\n", Código2);
@@ -81,8 +92,10 @@ int main() {
     printf("Nome da Cidade: %s", Cidade2);
     printf("População: %d\n", População2);
     printf("Área: %.2f Km²\n", Área2);
-    printf("PIB: %.2f\n", PIB2);
+    printf("PIB: %f\n", PIB2);
     printf("Número de Pontos Turísticos: %d\n", P_turístico2);
+    printf("Densidade Populacional: %f hab/Km²\n", Densidade2);
+    printf("PIB per capita: %f\n", PIBpercapita2);
 
     return 0;
 }
